30eb07958800d97145e67872525d06adde8bbac7
= <=>
2024-04-13 20:52:34 +0300

    Navbar Component Oluşturuldu.

--- bestorek-tr/src/main.js
+++ bestorek-tr/src/main.js
@@ -1,5 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
