fd1cb1889e373977139a59c9a5c69b4d73c366f5
= <=>
2025-01-31 20:56:32 +0300

    changes V2

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://vercel.com/bilals-projects-a4cd0321/bostorek-tr/5LYVj2LhpUaESCEqbFP6iD7mtoYY"],
   credentials: true,
 };
 
