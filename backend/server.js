8db72d3e989cbd0059a454be3f2559539e547666
= <=>
2025-06-01 16:31:00 +0300

    changes

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = 3000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://bostorek-tr-git-main-bilals-projects-a4cd0321.vercel.app/"],
+  origin: ["http://localhost:5173", "https://bostorek-tr-git-main-bilals-projects-a4cd0321.vercel.app"],
   credentials: true,
 };
 
